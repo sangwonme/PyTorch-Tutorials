94e70994561da292f370040afafbbc0f96038e6a
sangwonme <paulmoguri@naver.com>
2022-06-30 16:06:30 +0900

    :sparkles: Add Ch5.backPropagation

---
+++ Basics/ch5_backPropagation.py
@@ -0,0 +1,27 @@
+import torch
+
+# requires_grad ables to compute gradient of params
+x = torch.ones(5)  # input tensor
+y = torch.zeros(3)  # expected output
+w = torch.randn(5, 3, requires_grad=True)
+b = torch.randn(3, requires_grad=True)
+z = torch.matmul(x, w)+b
+loss = torch.nn.functional.binary_cross_entropy_with_logits(z, y)
+
+# get gradient by calling loss.backward()
+loss.backward()
+print(w.grad)
+print(b.grad)
+
+#  when only forward is required, quit tracking gradient
+z = torch.matmul(x, w)+b
+print(z.requires_grad)
+
+with torch.no_grad():
+    z = torch.matmul(x, w)+b
+print(z.requires_grad)
+
+z = torch.matmul(x, w)+b
+z_det = z.detach()
+print(z_det.requires_grad)
+
